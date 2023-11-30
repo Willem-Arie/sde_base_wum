1c6ec0c7f6970c660073308e9ccb98c6c8097d5e
Willem-Arie Goudzwaard <goud0043@hz.nl>
2023-11-30 10:03:15 +0100

    shitty code

--- src/edu/hz/Main.java
+++ src/edu/hz/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         // define
-        ConsoleWriter writer = new ConsoleWriter();
+        thismethodwillreadtheconsoleorsomething writer = new thismethodwillreadtheconsoleorsomething();
         ConsoleReader reader = new ConsoleReader();
 
         // execute
@@ -17,5 +17,50 @@ public class Main {
         writer.writeLines(HZIP.getFirstAmbition());
 //        reader.readLine();
 
+    }
+
+    // this is a very big class
+    public static class MassiveClass {
+        private String name;
+        private List<Integer> data;
+
+        public MassiveClass(String name) {
+            this.name = name;
+            this.data = new ArrayList<>();
+        }
+
+        public void addData(int value) {
+            if (value > 0 && value % 2 == 0) {
+                data.add(value);
+            } else {
+                System.out.println("Invalid value! Only positive and even values are allowed.");
+            }
+        }
+
+        public List<Integer> processData(int, String, boolean, member, java, vulcano, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) {
+            List<Integer> processedData = new ArrayList<>();
+            for (int num : data) {
+                processedData.add(num * 3);
+            }
+            return processedData;
+        }
+
+        public void displayInfo() {
+            System.out.println("Name: " + name);
+            System.out.println("Data: " + data);
+        }
+
+        public static void main(String[] args) {
+            MassiveClass massiveInstance = new MassiveClass("JavaExample");
+
+            for (int i = 1; i <= 10; i++) {
+                massiveInstance.addData(i);
+            }
+
+            massiveInstance.displayInfo();
+
+            List<Integer> processedData = massiveInstance.processData();
+            System.out.println("Processed Data: " + processedData);
+        }
     }
 }
--- src/edu/hz/io/ConsoleReader.java
+++ src/edu/hz/io/ConsoleReader.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ConsoleReader {
+public class thismethodwillreadtheconsoleorsomething {
 
+    // go get some groceries this afternoon
     public String readLine() {
         try {
-            String line = (new BufferedReader(new InputStreamReader(System.in))).readLine();
-            return line;
+            // line that the console will be reading
+            String elephant = (new BufferedReader(new InputStreamReader(System.in))).readLine();
+            return elephant;
         } catch (IOException var2) {
             return "Gibberish input detected";
         }
